8c9fe9a0685230f28655e40b4f16582d81ef0409
christianturner <christianturner@hey.com>
2021-11-13 23:14:12 -0500

    relink Londrina

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -5,16 +5,6 @@ const Header = () => {
     <Head>
       <title>Nouns POAP</title>
       <link rel="icon" href="/favicon.ico" />
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link
-        rel="preconnect"
-        href="https://fonts.gstatic.com"
-        crossOrigin="true"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Londrina+Solid:wght@100;300;400;900&display=swap"
-        rel="stylesheet"
-      />
     </Head>
   );
 };
