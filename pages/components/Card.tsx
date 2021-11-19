bc5c78e7f968b023f6c03fd0da1637c6f7499329
christianturner <christianturner@hey.com>
2021-11-19 11:56:39 -0500

    final styles fix

--- pages/components/Card.tsx
+++ pages/components/Card.tsx
@@ -9,9 +9,9 @@ const Card = ({ number, bidder }: CardProps) => {
   console.log("askdjfa", number, bidder);
   return (
     <div className="sm:border-b xs:w-full sm:w-1/2 border-nouns-yellow sm:border-r border-opacity-20 xs:px-2 xs:pl-6 sm:px-16 py-8 card">
-      <div className="sm:flex sm:flex-row-reverse">
+      <div className="sm:flex sm:flex-col lg:flex-row-reverse ">
         {/* <div className="card-group "> */}
-        <div className="sm:w-1/2">
+        <div className="sm:w-full lg:w-1/2">
           <a
             href={`https://nouns.wtf/auction/${number}`}
             target="_blank"
@@ -24,7 +24,7 @@ const Card = ({ number, bidder }: CardProps) => {
             />
           </a>
         </div>
-        <div className="text-nouns sm:w-1/2 xs:text-5xl  xs:pt-6 sm:pt-0  sm:text-5xl xs:mb-4 sm:mb-10 card-title w-full">
+        <div className="text-nouns sm:w-full lg:w-1/2 xs:text-5xl  xs:pt-6 sm:pt-10 lg:pt-0  sm:text-5xl xs:mb-4 sm:mb-10 card-title w-full ">
           Noun {number} POAP
           {/* </div> */}
           {/* </div> */}
