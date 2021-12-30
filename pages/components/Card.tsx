693e6c6c58d9c436d19f77427bbf0c5648398eb0
christianturner <christianturner@hey.com>
2021-12-30 11:31:54 -0500

    udpate

--- pages/components/Card.tsx
+++ pages/components/Card.tsx
@@ -21,24 +21,28 @@ const Card = ({ auction }: CardProps) => {
   // console.log("auction", auction);
 
   // Get winning bidder
-  const winningBidderAddr = auction.bidder.id;
+  const winningBidderAddr = auction && auction.bidder.id;
 
   // Filter the winning bidder out of bid info list
-  const filteredBidInfo = auction.bids.filter((bidInfo: any, id: number) => {
-    return bidInfo.bidder.id !== winningBidderAddr;
-  });
+  const filteredBidInfo =
+    auction &&
+    auction.bids.filter((bidInfo: any, id: number) => {
+      return bidInfo.bidder.id !== winningBidderAddr;
+    });
 
   // Construct map relating address => highestBidAmount
   const bidMap = new Map<string, number>();
-  filteredBidInfo.forEach((bidInfo: any) => {
-    if (bidMap.has(bidInfo.bidder.id)) {
-      if (bidMap.get(bidInfo.bidder.id) < Number(bidInfo.amount)) {
+
+  filteredBidInfo &&
+    filteredBidInfo.forEach((bidInfo: any) => {
+      if (bidMap.has(bidInfo.bidder.id)) {
+        if (bidMap.get(bidInfo.bidder.id) < Number(bidInfo.amount)) {
+          bidMap.set(bidInfo.bidder.id, Number(bidInfo.amount));
+        }
+      } else {
         bidMap.set(bidInfo.bidder.id, Number(bidInfo.amount));
       }
-    } else {
-      bidMap.set(bidInfo.bidder.id, Number(bidInfo.amount));
-    }
-  });
+    });
 
   // Sort by bid amount and take top 5
   const addressesGettingPOAP = Array.from(bidMap, ([address, amount]) => ({
@@ -53,41 +57,44 @@ const Card = ({ auction }: CardProps) => {
   // setBiddersGettingPOAP(addressesGettingPOAP);
   console.log("addressesGettingPOAP", addressesGettingPOAP);
 
-  return (
-    addressesGettingPOAP && (
-      <div className="sm:border-b xs:w-full sm:w-1/2 border-nouns-yellow sm:border-r border-opacity-20 xs:px-2 xs:pl-6 sm:px-16 py-8 card">
-        <div className="sm:flex sm:flex-col lg:flex-row-reverse ">
-          {/* <div className="card-group "> */}
-          <div className="sm:w-full lg:w-1/2">
-            <a
-              href={`https://nouns.wtf/auction/${auction.id}`}
-              // href={`https://nouns.wtf/auction/${number}`}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img
-                className=" m-auto h-52 z-10 card-image sm:cursor-pointer rounded-full"
-                src={`./beings/n${auction.id}.png`}
-                alt={`poap${auction.id}`}
-              />
-            </a>
-          </div>
-          <div className="text-nouns sm:w-full lg:w-1/2 xs:text-5xl  xs:pt-6 sm:pt-10 lg:pt-0  sm:text-5xl xs:mb-4 sm:mb-10 card-title w-full ">
-            Noun {auction.id} POAP
-            {/* </div> */}
-            {/* </div> */}
-            <div className="text-sm xs:leading-8 sm:leading-loose opacity-75 font-normal text-dm sm:pt-4">
-              {addressesGettingPOAP &&
-                addressesGettingPOAP.map((a) => (
-                  <p key={uuidv4()}> {shortAddress(a.address)}</p>
-                ))}
-            </div>
-          </div>
-          {/* </div> */}
+  return addressesGettingPOAP && auction ? (
+    <div className="sm:border-b xs:w-full sm:w-1/2 border-nouns-yellow sm:border-r border-opacity-20 xs:px-2 xs:pl-6 sm:px-16 py-8 card">
+      <div className="sm:flex sm:flex-col lg:flex-row-reverse ">
+        {/* <div className="card-group "> */}
+        <div className="sm:w-full lg:w-1/2">
+          <a
+            href={`https://nouns.wtf/auction/${auction.id ? auction.id : ""}`}
+            // href={`https://nouns.wtf/auction/${number}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className=" m-auto h-52 z-10 card-image sm:cursor-pointer rounded-full"
+              src={`./beings/n${auction.id ? auction.id : ""}.png`}
+              alt={`poap${auction.id}`}
+            />
+          </a>
         </div>
+        <div className="text-nouns sm:w-full lg:w-1/2 xs:text-5xl  xs:pt-6 sm:pt-10 lg:pt-0  sm:text-5xl xs:mb-4 sm:mb-10 card-title w-full ">
+          Noun {auction.id ? auction.id : ""} POAP
+          {/* </div> */}
+          {/* </div> */}
+          <div className="text-sm xs:leading-8 sm:leading-loose opacity-75 font-normal text-dm sm:pt-4">
+            {addressesGettingPOAP &&
+              auction &&
+              addressesGettingPOAP.map((a) => (
+                <p key={uuidv4()}>
+                  {" "}
+                  {shortAddress(a.address ? a.address : "")}
+                </p>
+              ))}
+          </div>
+        </div>
+        {/* </div> */}
       </div>
-    )
-    // <></>
+    </div>
+  ) : (
+    <></>
   );
 };
 
